f417486cba8c8baad9f076ad0444abbb087adcbe
Komal <divyavairagade@gmail.com>
2022-05-03 00:23:16 +0530

    UC8 is added using class method

---
+++ Assign_08/Class8.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assign_08
+{
+    internal class Class8
+    {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+
+        public static int ComputeEmpWage(string Company, int EmpRatePerHour, int NumOfWorkingDays, int MaxHoursPerMonth)
+        {
+            //Variables
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+            while (totalEmpHrs <= MaxHoursPerMonth && totalWorkingDays < NumOfWorkingDays)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day#:" + totalWorkingDays + "Emp Hrs :" + empHrs);
+            }
+            int totalEmpWage = totalEmpHrs * EmpRatePerHour;
+            Console.WriteLine("Total Emp Wage :" + totalEmpWage);
+            return totalEmpWage;
+        }
+    }
+}
--- Assign_08/Program.cs
+++ Assign_08/Program.cs
@@ -2,10 +2,12 @@
 using Assign_08;
 
 Console.WriteLine("Welcome to the Employee wage computation program");
-Class1.EmployeeAttendance();
-Class2.EmployeeWagesCal();
-Class3.PartTimeEmpWages();
-Class4.PartTimeUsingSwitch();
-Class5.WagesForMonth();
-Class6.TotalEmployeeWage();
-Class7.ComputeEmpWage();
+//Class1.EmployeeAttendance();
+//Class2.EmployeeWagesCal();
+//Class3.PartTimeEmpWages();
+//Class4.PartTimeUsingSwitch();
+//Class5.WagesForMonth();
+//Class6.TotalEmployeeWage();
+//Class7.ComputeEmpWage();
+Class8.ComputeEmpWage("Dmart", 20, 2, 10);
+Class8.ComputeEmpWage("Reliance", 10, 4, 20);
