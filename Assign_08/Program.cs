f114c122fa30a66a134c5ae8471cbbac5295fc91
Komal <divyavairagade@gmail.com>
2022-05-03 20:36:08 +0530

    Interface approach for various company

--- Assign_08/Program.cs
+++ Assign_08/Program.cs
@@ -13,10 +13,15 @@ Console.WriteLine("Welcome to the Employee wage computation program");
 //Class8.ComputeEmpWage("Dmart", 20, 2, 10);
 //Class8.ComputeEmpWage("Reliance", 10, 4, 20);
 
-EmpWageBuilder Dmart = new EmpWageBuilder("Dmart", 20, 2, 10);
-EmpWageBuilder Reliance = new EmpWageBuilder("Reliance", 10, 4, 20);
-Dmart.ComputeWage();
-Console.WriteLine(Dmart.ToString());
-Reliance.ComputeWage();
-Console.WriteLine(Reliance.ToString());
+//EmpWageBuilder Dmart = new EmpWageBuilder("Dmart", 20, 2, 10);
+//EmpWageBuilder Reliance = new EmpWageBuilder("Reliance", 10, 4, 20);
+//Dmart.ComputeWage();
+//Console.WriteLine(Dmart.ToString());
+//Reliance.ComputeWage();
+//Console.WriteLine(Reliance.ToString());
+
+EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+empWageBuilder.addCompanyEmpWage("Dmart", 20, 2, 10);
+empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
+empWageBuilder.ComputeEmpWage();
 
